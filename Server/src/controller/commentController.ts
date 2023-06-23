3c976ac07ef1a445baf2005a07d74fa044b7c965
Idongesit Ukpong <116917866+IdyUkpong@users.noreply.github.com>
2023-06-23 23:20:20 +0100

    community model updated

--- Server/src/controller/commentController.ts
+++ Server/src/controller/commentController.ts
@@ -5,36 +5,36 @@ import { v4 as uuidv4 } from "uuid";
 import Post from "../models/postModel";
 import User from "../models/userModel";
 
-export const createComment = async (req: JwtPayload, res: Response) => {
-    try{
-        const id = uuidv4();
+// export const createComment = async (req: JwtPayload, res: Response) => {
+//     try{
+//         const id = uuidv4();
         
-        const userId = req.user.id
-        const {postId, comment} = req.body;
+//         const userId = req.user.id
+//         const {postId, comment} = req.body;
 
-        const post = await Post.findOne({
-            where: {
-                id: postId
-            }
-        });
-        if (!post){
-            return res.status(400).json({message: "post does not exist"})
-        }
-        const newComment = await Comment.create({
-            id,
-            postId: postId,
-            userId: userId,
-            comment,
-            like: [],
-        });
-        await Post.increment(['comment'], { by: 1, where: { id: postId }});
-        res.status(201).json({comment: newComment});
+//         const post = await Post.findOne({
+//             where: {
+//                 id: postId
+//             }
+//         });
+//         if (!post){
+//             return res.status(400).json({message: "post does not exist"})
+//         }
+//         const newComment = await Comment.create({
+//             id,
+//             postId: postId,
+//             userId: userId,
+//             comment,
+//             like: [],
+//         });
+//         await Post.increment(['comment'], { by: 1, where: { id: postId }});
+//         res.status(201).json({comment: newComment});
         
-    }catch(err){
-        console.error('Error creating comment', err);
-        res.status(500).json({Error: 'Failed to create comment'});
-    }
-};
+//     }catch(err){
+//         console.error('Error creating comment', err);
+//         res.status(500).json({Error: 'Failed to create comment'});
+//     }
+// };
 
 
 export const fetchComments = async (req: Request, res: Response) => {
@@ -102,29 +102,29 @@ try {
 //     }
 //   }
 
-export const deleteComment = async (req: JwtPayload, res: Response) => {
-    const {commentId} = req.params;
-    const userId = req.user.id
-    console.log(commentId);
-    console.log(userId);
+// export const deleteComment = async (req: JwtPayload, res: Response) => {
+//     const {commentId} = req.params;
+//     const userId = req.user.id
+//     console.log(commentId);
+//     console.log(userId);
 
-    const comment:Comment | any = await Comment.findOne({
-        where: {
-            id: commentId,
-            userId: userId
-        }
-    })
-    if(comment){
-        Comment.destroy({
-            where: {
-                id: commentId,
-                userId: userId
-            }
-        })
-        await Post.decrement('comment', { by: 1, where: { id: comment.post_id }});
-        res.status(201).json({message: "comment deleted successfully"});
-    } else {
-        res.status(400).json({Error: 'cannot  delete comment'})
-    }
+//     const comment:Comment | any = await Comment.findOne({
+//         where: {
+//             id: commentId,
+//             userId: userId
+//         }
+//     })
+//     if(comment){
+//         Comment.destroy({
+//             where: {
+//                 id: commentId,
+//                 userId: userId
+//             }
+//         })
+//         await Post.decrement('comment', { by: 1, where: { id: comment.post_id }});
+//         res.status(201).json({message: "comment deleted successfully"});
+//     } else {
+//         res.status(400).json({Error: 'cannot  delete comment'})
+//     }
 
-}
+// }
--- Server/src/models/commuintyModel.ts
+++ Server/src/models/commuintyModel.ts
@@ -3,63 +3,64 @@ import { sequelize } from '../config/database';
 import User from './userModel';
 
 export interface CommunityAttributes {
-    id:string;
-    userId:string;
-    walletId:string;
-    communityName: string;
-    about:string;
-    users:string[];
+  id: string;
+  userId: string;
+  walletId: string;
+  communityName: string;
+  about: string;
+  users: string[];
 }
 
 class Community extends Model<CommunityAttributes> {
-    id!:string;
-    userId!:string;
-    walletId!:string;
-    communityName!: string;
-    about!:string;
-    users!:string[];
-    createdAt: any;
-    updatedAt: any;
-  }
+  id!: string;
+  userId!: string;
+  walletId!: string;
+  communityName!: string;
+  about!: string;
+  users!: string[];
+  createdAt!: Date;
+  updatedAt!: Date;
+}
 
-  Community.init({
+Community.init(
+  {
     id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true,
-        allowNull: false,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+    userId: {
+      type: DataTypes.UUID, // Update the data type to DataTypes.UUID
+      allowNull: false,
+      references: {
+        model: User,
+        key: 'id',
       },
-      userId: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        references: {
-          model: User,
-          key: 'id',
-        },
-      },
-      walletId: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      communityName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-      },
-      
-      about: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      users: {
-        type: DataTypes.ARRAY(DataTypes.UUID),
-        defaultValue: [],
-        },
+    },
+    walletId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    communityName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    about: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    users: {
+      type: DataTypes.ARRAY(DataTypes.UUID),
+      defaultValue: [],
+    },
   },
   {
     sequelize,
     modelName: 'Community',
     timestamps: true,
-  })
+  }
+);
 
-  export default Community
+export default Community;
